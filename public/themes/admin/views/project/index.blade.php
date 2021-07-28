c46c769bbd35cedfc6a1618838103d9704f4230e
wzj <wzj@344f81b5-1e9b-a94b-9f2f-86fa34238b16>
2021-07-28 11:37:21 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yuanxin@43 344f81b5-1e9b-a94b-9f2f-86fa34238b16

--- public/themes/admin/views/project/index.blade.php
+++ public/themes/admin/views/project/index.blade.php
@@ -38,7 +38,7 @@
                 ,{field:'id',title:'ID', width:80, sort: true}
                 ,{field:'name',title:'{{ trans('project.label.name') }}',edit:'text'}
                 ,{field:'image',title:'{{ trans('app.image') }}', width:200,toolbar:'#imageTEM',}
-                ,{field:'description',title:'{{ trans('app.description') }}'}
+                ,{field:'description',title:'{{ trans('app.description') }}',edit:'text'}
                 ,{field:'order',title:'{{ trans('app.order') }}', sort: true}
                 ,{field:'score',title:'{{ trans('app.actions') }}', width:200, align: 'right',toolbar:'#barDemo'}
             ]]
